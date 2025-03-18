de4166266e9e3c66aba5ffb532557d236b5d7a88
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-18 20:29:10 -0300

    fix(controller): rename package

--- anime-service/src/main/java/dev/w2k/animeservice/controller/HelloController.java
+++ anime-service/src/main/java/dev/w2k/animeservice/controller/HelloController.java
@@ -1,4 +1,4 @@
-package dev.w2k.animeservice.controllers;
+package dev.w2k.animeservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
