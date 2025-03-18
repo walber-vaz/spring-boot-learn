fbbe81ee98c7f3360f86f41b956a2005059b88ae
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-18 20:51:31 -0300

    feat(config): view inject bean startup project and default package scan

--- anime-service/src/main/java/dev/w2k/animeservice/AnimeServiceApplication.java
+++ anime-service/src/main/java/dev/w2k/animeservice/AnimeServiceApplication.java
@@ -4,9 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*
+ * @ComponentScan(basePackages = "dev.w2k.test")
+ *
+ * Para fazer o scan de pacotes diferentes do pacote onde a classe principal está
+ * Isso vai sobrescrever o scan padrão feito no pacote onde a classe principal está
+ * Caso queira adicionar mais de um pacote, basta adicionar mais um pacote no array
+ * Exemplo:
+ * @ComponentScan(basePackages = {"dev.w2k.test", "dev.w2k.animeservice"})
+ **/
 public class AnimeServiceApplication {
 
   public static void main(String[] args) {
+    /*
+     * Esse codigo é responsavel por iniciar a aplicação Spring Boot
+     * e imprimir no console todos os beans que foram inicializados
+     *
+     * var applicationContext = SpringApplication.run(AnimeServiceApplication.class, args);
+     * Arrays.stream(applicationContext.getBeanDefinitionNames())
+     *     .sorted()
+     *     .forEach(System.out::println);
+     **/
     SpringApplication.run(AnimeServiceApplication.class, args);
   }
 
