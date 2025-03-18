384a3154eb0a38f7e6bd7c8fd45b5627de7a4f3c
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-18 18:26:23 -0300

    chore(init): initial project

---
+++ anime-service/src/test/java/dev/w2k/animeservice/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.w2k.animeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
