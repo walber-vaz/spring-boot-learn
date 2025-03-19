5a2cec0434ad6e8335a1b060bfff6c0402ff1565
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-19 15:44:26 -0300

    feat(domain): enhance Anime class with builder pattern and createdAt field

--- anime-service/src/main/java/dev/w2k/animeservice/domain/Anime.java
+++ anime-service/src/main/java/dev/w2k/animeservice/domain/Anime.java
@@ -1,23 +1,42 @@
 package dev.w2k.animeservice.domain;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class Anime {
+
   private Long id;
   private String name;
+  private LocalDateTime createdAt;
 
   @Getter
   private static List<Anime> animes = new ArrayList<>();
   static {
-    animes.add(new Anime(ThreadLocalRandom.current().nextLong(1, 1000), "Naruto"));
-    animes.add(new Anime(ThreadLocalRandom.current().nextLong(1, 1000), "Dragon Ball"));
-    animes.add(new Anime(ThreadLocalRandom.current().nextLong(1, 1000), "One Piece"));
+    var naruto = Anime.builder()
+        .id(1L)
+        .name("Naruto")
+        .createdAt(LocalDateTime.now())
+        .build();
+    var dragonBall = Anime.builder()
+        .id(2L)
+        .name("Dragon Ball")
+        .createdAt(LocalDateTime.now())
+        .build();
+    var onePiece = Anime.builder()
+        .id(3L)
+        .name("One Piece")
+        .createdAt(LocalDateTime.now())
+        .build();
+
+    animes.addAll(List.of(naruto, dragonBall, onePiece));
   }
 }
