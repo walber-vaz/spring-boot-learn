8ff50bb5aeb64c71c53fb32a52789843660b7484
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-19 14:54:45 -0300

    feat(Producer): enhance Producer class with builder pattern and createdAt field

--- anime-service/src/main/java/dev/w2k/animeservice/domain/Producer.java
+++ anime-service/src/main/java/dev/w2k/animeservice/domain/Producer.java
@@ -1,28 +1,44 @@
 package dev.w2k.animeservice.domain;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Producer {
+
   private Long id;
   /*
-  * @JsonProperty("full_name")
-  * Quando queremos que o nome do atributo seja diferente do nome do campo no JSON
-  * */
+   * @JsonProperty("full_name")
+   * Quando queremos que o nome do atributo seja diferente do nome do campo no JSON
+   * */
   private String name;
+  private LocalDateTime createdAt;
 
   @Getter
   private static List<Producer> producers = new ArrayList<>();
+
   static {
-    var madhouse = new Producer(ThreadLocalRandom.current().nextLong(1, 1000), "Madhouse");
-    var sunrise = new Producer(ThreadLocalRandom.current().nextLong(1, 1000), "Sunrise");
-    var toeiAnimation = new Producer(ThreadLocalRandom.current().nextLong(1, 1000), "Toei Animation");
+    var madhouse = Producer.builder()
+        .id(1L)
+        .name("Madhouse")
+        .createdAt(LocalDateTime.now())
+        .build();
+    var sunrise = Producer.builder()
+        .id(2L)
+        .name("Sunrise")
+        .createdAt(LocalDateTime.now())
+        .build();
+    var toeiAnimation = Producer.builder()
+        .id(3L)
+        .name("Toei Animation")
+        .createdAt(LocalDateTime.now())
+        .build();
 
     producers.addAll(List.of(madhouse, sunrise, toeiAnimation));
   }
