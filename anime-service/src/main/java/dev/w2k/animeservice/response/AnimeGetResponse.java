a07499ec588a96d02ea0e2704fc41753eca0f6d0
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-19 16:27:29 -0300

    feat(response): add id field to AnimeGetResponse class

--- anime-service/src/main/java/dev/w2k/animeservice/response/AnimeGetResponse.java
+++ anime-service/src/main/java/dev/w2k/animeservice/response/AnimeGetResponse.java
@@ -1,12 +1,11 @@
 package dev.w2k.animeservice.response;
 
-import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class AnimeGetResponse {
+  private Long id;
   private String name;
-  private LocalDateTime createdAt;
 }
