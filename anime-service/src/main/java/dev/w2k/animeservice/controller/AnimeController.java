9df213f06aaeaa380335539adee957ca1cc6c743
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-19 09:54:51 -0300

    feat(controller): update AnimeController to return Anime objects and add filtering by name and ID

--- anime-service/src/main/java/dev/w2k/animeservice/controller/AnimeController.java
+++ anime-service/src/main/java/dev/w2k/animeservice/controller/AnimeController.java
@@ -1,28 +1,61 @@
 package dev.w2k.animeservice.controller;
 
+import dev.w2k.animeservice.domain.Anime;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("v1/animes")
 @Slf4j
 public class AnimeController {
-
   /*
-   * Create a controller and a method that returns a list of anime names though the following
-   * endpoint: http://localhost:8080/v1/animes
-   **/
+  *
+  * @GetMapping
+  * public List<String> listAll() throws InterruptedException {
+  *   Simulate a slow request, so we can see the benefits of using a custom thread pool.
+  *
+  *   log.info(Thread.currentThread().getName());
+  *   TimeUnit.SECONDS.sleep(1);
+  *   return List.of("Dragon Ball", "Naruto", "One Piece");
+  * }
+  * */
+
   @GetMapping
-  public List<String> listAll() throws InterruptedException {
-    /*
-     * Simulate a slow request, so we can see the benefits of using a custom thread pool.
-     * */
-    log.info(Thread.currentThread().getName());
-    TimeUnit.SECONDS.sleep(1);
-    return List.of("Dragon Ball", "Naruto", "One Piece");
+  public List<Anime> listAll() {
+    log.info("Get all animes");
+    return Anime.getAnimes();
+  }
+
+  @GetMapping("/filter")
+  public Anime findByName(@RequestParam String name) {
+    if (name == null || name.isBlank()) {
+      log.warn("Anime not found");
+      return null;
+    }
+
+    log.info("Searching for anime: {}", name);
+    return Anime.getAnimes().stream()
+        .filter(anime -> anime.getName().equalsIgnoreCase(name))
+        .findFirst()
+        .orElse(null);
+  }
+
+  @GetMapping("{id}")
+  public Anime findById(@PathVariable Long id) {
+    if (id == null) {
+      log.warn("Anime not found");
+      return null;
+    }
+
+    log.info("Searching for anime with id: {}", id);
+    return Anime.getAnimes().stream()
+        .filter(anime -> anime.getId().equals(id))
+        .findFirst()
+        .orElse(null);
   }
 }
