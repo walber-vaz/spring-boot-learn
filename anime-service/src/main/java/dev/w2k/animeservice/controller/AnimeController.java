adc5261745035e9407e2aeee58386be0a53e2e7e
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-18 21:44:06 -0300

    feat(controller): simulate slow request in listAll() method for testing custom thread pool

--- anime-service/src/main/java/dev/w2k/animeservice/controller/AnimeController.java
+++ anime-service/src/main/java/dev/w2k/animeservice/controller/AnimeController.java
@@ -1,12 +1,15 @@
 package dev.w2k.animeservice.controller;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("v1/animes")
+@Slf4j
 public class AnimeController {
 
   /*
@@ -14,7 +17,12 @@ public class AnimeController {
    * endpoint: http://localhost:8080/v1/animes
    **/
   @GetMapping
-  public List<String> listAll() {
+  public List<String> listAll() throws InterruptedException {
+    /*
+     * Simulate a slow request, so we can see the benefits of using a custom thread pool.
+     * */
+    log.info(Thread.currentThread().getName());
+    TimeUnit.SECONDS.sleep(1);
     return List.of("Dragon Ball", "Naruto", "One Piece");
   }
 }
