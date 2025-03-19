d18e8d08a0c5b58f4b9aaccec5e4fb3032b4d6a1
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-19 16:32:55 -0300

    feat(request): add toString method to AnimePostRequest class

--- anime-service/src/main/java/dev/w2k/animeservice/request/AnimePostRequest.java
+++ anime-service/src/main/java/dev/w2k/animeservice/request/AnimePostRequest.java
@@ -2,9 +2,11 @@ package dev.w2k.animeservice.request;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class AnimePostRequest {
   private String name;
 }
