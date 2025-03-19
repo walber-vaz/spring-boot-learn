4575ea051194ebd625aa59b7ea4580b97ffdb5ee
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-19 09:34:53 -0300

    feat(controller): add findByName endpoint to HeroController for hero lookup by name

--- anime-service/src/main/java/dev/w2k/animeservice/controller/HeroController.java
+++ anime-service/src/main/java/dev/w2k/animeservice/controller/HeroController.java
@@ -3,6 +3,7 @@ package dev.w2k.animeservice.controller;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +34,13 @@ public class HeroController {
     return HEROES.stream()
         .filter(hero -> names.stream().anyMatch(name -> hero.toLowerCase().contains(name.toLowerCase())))
         .toList();
+  }
+
+  @GetMapping("{name}")
+  public String findByName(@PathVariable String name) {
+    return HEROES.stream()
+        .filter(hero -> hero.equalsIgnoreCase(name))
+        .findFirst()
+        .orElse("Hero not found");
   }
 }
