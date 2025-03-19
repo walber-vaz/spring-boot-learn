9a2ed90e3e4159d924a9f8140978da8d9663f901
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-19 10:44:54 -0300

    feat(controller): add save endpoint to HelloController for saving names

--- anime-service/src/main/java/dev/w2k/animeservice/controller/HelloController.java
+++ anime-service/src/main/java/dev/w2k/animeservice/controller/HelloController.java
@@ -1,13 +1,26 @@
 package dev.w2k.animeservice.controller;
 
+import java.util.concurrent.ThreadLocalRandom;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("v1/greetings")
+@Slf4j
 public class HelloController {
 
-  @GetMapping("hello")
+  @GetMapping
   public String hello() {
     return "Hello, World!";
+  }
+
+  @PostMapping
+  public Long save(@RequestBody String name) {
+    log.info("Saving name: {}", name);
+    return ThreadLocalRandom.current().nextLong(1, 1000);
   }
 }
