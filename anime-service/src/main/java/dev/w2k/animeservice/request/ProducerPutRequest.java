10e1c8e3571123e500ad82ed441cc18b53c7458f
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-20 09:18:31 -0300

    feat(request): add ProducerPutRequest class for updating producer details

---
+++ anime-service/src/main/java/dev/w2k/animeservice/request/ProducerPutRequest.java
@@ -0,0 +1,11 @@
+package dev.w2k.animeservice.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ProducerPutRequest {
+  private Long id;
+  private String name;
+}
