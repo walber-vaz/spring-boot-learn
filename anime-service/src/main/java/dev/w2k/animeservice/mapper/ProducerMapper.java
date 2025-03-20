399636bccf8ecb9dc1721a5df53d9120a097fc11
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-20 09:18:25 -0300

    feat(mapper): add mapping method for ProducerPutRequest to ProducerMapper

--- anime-service/src/main/java/dev/w2k/animeservice/mapper/ProducerMapper.java
+++ anime-service/src/main/java/dev/w2k/animeservice/mapper/ProducerMapper.java
@@ -2,6 +2,7 @@ package dev.w2k.animeservice.mapper;
 
 import dev.w2k.animeservice.domain.Producer;
 import dev.w2k.animeservice.request.ProducerPostRequest;
+import dev.w2k.animeservice.request.ProducerPutRequest;
 import dev.w2k.animeservice.response.ProducerGetResponse;
 import java.util.List;
 import org.mapstruct.Mapper;
@@ -13,11 +14,14 @@ public interface ProducerMapper {
 
   ProducerMapper INSTANCE = Mappers.getMapper(ProducerMapper.class);
 
-  @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
   @Mapping(target = "id", expression = "java(java.util.concurrent.ThreadLocalRandom.current().nextLong(1, 1000))")
   Producer toProducer(ProducerPostRequest producerRequest);
+
+  @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
+  Producer toProducer(ProducerPutRequest request);
 
   ProducerGetResponse toProducerGetResponse(Producer producer);
 
   List<ProducerGetResponse> toProducerGetResponseList(List<Producer> producers);
+
 }
