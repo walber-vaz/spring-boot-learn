825b0099aa28f93bcbacd45ac7f1e71df7b9a49c
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-20 09:10:52 -0300

    feat(mapper): add mapping method for AnimePutRequest to AnimeMapper

--- anime-service/src/main/java/dev/w2k/animeservice/mapper/AnimeMapper.java
+++ anime-service/src/main/java/dev/w2k/animeservice/mapper/AnimeMapper.java
@@ -2,6 +2,7 @@ package dev.w2k.animeservice.mapper;
 
 import dev.w2k.animeservice.domain.Anime;
 import dev.w2k.animeservice.request.AnimePostRequest;
+import dev.w2k.animeservice.request.AnimePutRequest;
 import dev.w2k.animeservice.response.AnimeGetResponse;
 import java.util.List;
 import org.mapstruct.Mapper;
@@ -15,8 +16,10 @@ public interface AnimeMapper {
 
   @Mapping(target = "id", expression = "java(java.util.concurrent.ThreadLocalRandom.current().nextLong(1, 1000))")
   Anime toAnime(AnimePostRequest animePostRequest);
+  Anime toAnime(AnimePutRequest request);
 
   AnimeGetResponse toAnimeGetResponse(Anime anime);
 
   List<AnimeGetResponse> toAnimeGetResponseList(List<Anime> animes);
+
 }
