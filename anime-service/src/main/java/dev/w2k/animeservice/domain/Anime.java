0a0fdbb0c3ad189eebb3ecc15d7e95c86b8a7066
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-20 16:10:55 -0300

    refactor(domain): simplify Anime class by removing static list and unused annotations

--- anime-service/src/main/java/dev/w2k/animeservice/domain/Anime.java
+++ anime-service/src/main/java/dev/w2k/animeservice/domain/Anime.java
@@ -1,42 +1,19 @@
 package dev.w2k.animeservice.domain;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
 @Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Anime {
 
+  @EqualsAndHashCode.Include
   private Long id;
   private String name;
   private LocalDateTime createdAt;
-
-  @Getter
-  private static List<Anime> animes = new ArrayList<>();
-
-  static {
-    var naruto = Anime.builder()
-        .id(1L)
-        .name("Naruto")
-        .createdAt(LocalDateTime.now())
-        .build();
-    var dragonBall = Anime.builder()
-        .id(2L)
-        .name("Dragon Ball")
-        .createdAt(LocalDateTime.now())
-        .build();
-    var onePiece = Anime.builder()
-        .id(3L)
-        .name("One Piece")
-        .createdAt(LocalDateTime.now())
-        .build();
-
-    animes.addAll(List.of(naruto, dragonBall, onePiece));
-  }
 }
