914b120147cda176717b5c933110c8b0b02ebb92
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-20 09:43:48 -0300

    feat(request): add @ToString annotation to ProducerPostRequest and ProducerPutRequest classes

--- anime-service/src/main/java/dev/w2k/animeservice/request/ProducerPutRequest.java
+++ anime-service/src/main/java/dev/w2k/animeservice/request/ProducerPutRequest.java
@@ -2,9 +2,11 @@ package dev.w2k.animeservice.request;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@ToString
 public class ProducerPutRequest {
   private Long id;
   private String name;
