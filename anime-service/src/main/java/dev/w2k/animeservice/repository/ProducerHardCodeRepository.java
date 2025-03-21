636bda6ffcb9a99077305b615af1a54da5a914b7
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-20 22:49:25 -0300

    feat(repository): refactor ProducerHardCodeRepository to use ProducerData for producer management

--- anime-service/src/main/java/dev/w2k/animeservice/repository/ProducerHardCodeRepository.java
+++ anime-service/src/main/java/dev/w2k/animeservice/repository/ProducerHardCodeRepository.java
@@ -2,8 +2,6 @@ package dev.w2k.animeservice.repository;
 
 import dev.w2k.animeservice.domain.Producer;
 import external.dependency.Connection;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -16,55 +14,35 @@ import org.springframework.stereotype.Repository;
 public class ProducerHardCodeRepository {
 
   private final Connection connection;
-  private static final List<Producer> PRODUCERS = new ArrayList<>();
-
-  static {
-    var madhouse = Producer.builder()
-        .id(1L)
-        .name("Madhouse")
-        .createdAt(LocalDateTime.now())
-        .build();
-    var sunrise = Producer.builder()
-        .id(2L)
-        .name("Sunrise")
-        .createdAt(LocalDateTime.now())
-        .build();
-    var toeiAnimation = Producer.builder()
-        .id(3L)
-        .name("Toei Animation")
-        .createdAt(LocalDateTime.now())
-        .build();
-
-    PRODUCERS.addAll(List.of(madhouse, sunrise, toeiAnimation));
-  }
+  private final ProducerData producerData;
 
   public List<Producer> findAll() {
     log.info(connection);
-    return PRODUCERS;
+    return producerData.getProducers();
   }
 
   public Optional<Producer> findById(Long id) {
-    return PRODUCERS
+    return producerData.getProducers()
         .stream()
         .filter(p -> p.getId().equals(id))
         .findFirst();
   }
 
   public List<Producer> findByName(String name) {
-    return PRODUCERS
+    return producerData.getProducers()
         .stream()
         .filter(a -> a.getName().toLowerCase().contains(name.toLowerCase()))
         .toList();
   }
 
   public Producer save(Producer producer) {
-    PRODUCERS.add(producer);
+    producerData.getProducers().add(producer);
 
     return producer;
   }
 
   public void delete(Producer producer) {
-    PRODUCERS.remove(producer);
+    producerData.getProducers().remove(producer);
   }
 
   public void update(Producer producer) {
