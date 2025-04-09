469158368e3a865209445c87edc33675373add90
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-04-09 15:36:08 -0300

    feat(model): add getter and setter methods to Connection class

--- anime-service/src/main/java/external/dependency/Connection.java
+++ anime-service/src/main/java/external/dependency/Connection.java
@@ -1,10 +1,14 @@
 package external.dependency;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @ToString
 @AllArgsConstructor
+@Getter
+@Setter
 public class Connection {
   private String host;
   private String username;
