4359b83f4b82e2b646cff07de3afd478a7275f23
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-04-09 14:44:37 -0300

    refactor(map): remove unused INSTANCE field from ProducerMapper

--- anime-service/src/main/java/dev/w2k/animeservice/mapper/ProducerMapper.java
+++ anime-service/src/main/java/dev/w2k/animeservice/mapper/ProducerMapper.java
@@ -8,13 +8,9 @@ import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ProducerMapper {
-
-  ProducerMapper INSTANCE = Mappers.getMapper(ProducerMapper.class);
-
   @Mapping(target = "id", expression = "java(java.util.concurrent.ThreadLocalRandom.current().nextLong(1, 1000))")
   @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
   Producer toProducer(ProducerPostRequest producerRequest);
