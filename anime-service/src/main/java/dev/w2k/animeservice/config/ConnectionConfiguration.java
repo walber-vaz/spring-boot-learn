efe22fb841c1746be2a0884df5a38ddca4da7037
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-04-09 15:36:02 -0300

    feat(config): update ConnectionConfiguration to use externalized database properties

--- anime-service/src/main/java/dev/w2k/animeservice/config/ConnectionConfiguration.java
+++ anime-service/src/main/java/dev/w2k/animeservice/config/ConnectionConfiguration.java
@@ -1,14 +1,21 @@
 package dev.w2k.animeservice.config;
 
 import external.dependency.Connection;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConnectionConfiguration {
+  @Value("${database.url}")
+  private String url;
+  @Value("${database.username}")
+  private String username;
+  @Value("${database.password}")
+  private String password;
 
   @Bean
   public Connection connection() {
-    return new Connection("localhost", "w2k", "password");
+    return new Connection(url, username, password);
   }
 }
