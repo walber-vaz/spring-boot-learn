a8764a9aabd5ec6dd641b81475d9aa227fd8140b
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-04-09 15:35:57 -0300

    feat(controller): add ConnectionController to fetch connection details

---
+++ anime-service/src/main/java/dev/w2k/animeservice/controller/ConnectionController.java
@@ -0,0 +1,23 @@
+package dev.w2k.animeservice.controller;
+
+import external.dependency.Connection;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/connections")
+@Slf4j
+@RequiredArgsConstructor
+public class ConnectionController {
+  private final Connection connection;
+
+  @GetMapping
+  public ResponseEntity<Connection> getConnection() {
+    log.info("Fetching connection details");
+    return ResponseEntity.ok(connection);
+  }
+}
