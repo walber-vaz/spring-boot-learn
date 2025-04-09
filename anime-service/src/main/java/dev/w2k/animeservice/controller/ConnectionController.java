5b616c1b62b8b5bcd19b5fdceb40f2c54cea135e
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-04-09 16:31:55 -0300

    fix(controller): update import statement for Connection class

--- anime-service/src/main/java/dev/w2k/animeservice/controller/ConnectionController.java
+++ anime-service/src/main/java/dev/w2k/animeservice/controller/ConnectionController.java
@@ -1,6 +1,6 @@
 package dev.w2k.animeservice.controller;
 
-import external.dependency.Connection;
+import dev.w2k.animeservice.config.Connection;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
